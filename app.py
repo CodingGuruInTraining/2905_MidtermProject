de9ba99a12b6dbd62c62adb3dc3dfb1cc2cf0858
MF <notsure@compuserv.com>
2017-10-14 14:02:30 -0500

    added buttons and directions for them

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+    return render_template('homebase.html')
 
+
+@app.route('/newtask', methods=['POST'])
+@app.route('/viewtasks', methods=['POST'])
+def task():
+    if request.form['taskButton'] == 'NEW TASK':
+        return render_template('newtask.html')
+    elif request.form['taskButton'] == 'VIEW TASKS':
+        return render_template('viewtasks.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
+
+
+
+# references:
+    # button redirects - https://stackoverflow.com/questions/19794695/flask-python-buttons
