5b3e4c030bdabd6c22c645c634def16d95f5ba5a
MF <notsure@compuserv.com>
2017-10-21 18:11:24 -0500

    made test data file for testing output later

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 from flask import Flask, render_template, url_for, request
+# from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+# db = SQLAlchemy(app)
+#
+# # TODO - Export class to py file:
+# class User(db.Model):
+#     __tablename__ = "notusers"
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(50), unique=True, nullable=False)
+#     firstname = db.Column(db.String(50))
+#     lastname = db.Column(db.String(75))
+#     points = db.Column(db.Integer)
+#     createdate = db.Column(db.DateTime)
+#
+#     def __repr__(self):
+#         return '<User %r>' % self.username
 
 @app.route('/')
 def home():
---
+++ tempdata.py
@@ -0,0 +1,33 @@
+def dataFunction():
+    datalist = [
+        {
+            'id': 1,
+            'taskname': 'steal candy from baby',
+            'descript': '',
+            'opened': '87 years',
+            'seller': 'Alucard\nLevel 99',
+            'fare': '12% of hall',
+            'duration': '',
+            'status': 'Active'
+        },
+        {
+            'id': 2,
+            'taskname': 'weekly backrub',
+            'descript': '',
+            'opened': '3 hours',
+            'seller': 'Barb\nLevel 2',
+            'fare': 'sheet of cookies/wk',
+            'duration': '',
+            'status': 'Active'
+        },
+        {
+            'id': 3,
+            'taskname': 'wash dishes',
+            'descript': 'there are SOOOO many!',
+            'opened': '13 minutes',
+            'seller': 'Cookie Monster\nLevel 32',
+            'fare': 'fruit basket',
+            'duration': '4 hours',
+            'status': 'Active'
+        }
+    ]
